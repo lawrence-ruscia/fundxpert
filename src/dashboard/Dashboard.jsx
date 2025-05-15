04b1beae09e61b001446f2e98fe976ddd99a5f2b
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-05-15 14:41:13 +0800

    rename dashboard chart component for clarity

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import { ChartAreaInteractive } from '@/dashboard/components/chart-area-interactive';
+import { ChartAreaInteractive } from '@/dashboard/components/fund-growth';
 import { DataTable } from '@/components/data-table';
 import { SectionCards } from '@/components/section-cards';
 import { SiteHeader } from '@/components/site-header';
