7af94ee6bc1fe290a56990c6601ff94a6932ff04
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-05-15 17:20:14 +0800

    adjust dashboard main padding

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -30,7 +30,7 @@ export default function Dashboard() {
         </Header>
         <div className='flex flex-1 flex-col'>
           <div className='@container/main flex flex-1 flex-col gap-2'>
-            <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
+            <div className='flex flex-col gap-4 py-3 md:gap-6 md:py-6'>
               <h1 className='px-4 lg:px-6 text-2xl font-bold'>Dashboard</h1>
               <SectionCards />
               <div className='grid grid-cols-1 gap-4 lg:grid-cols-7 px-4 lg:px-6'>
